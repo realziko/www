0fdd6e0f2cb00dfe88a62f4002d05074473af8e4
Azizbek Umarov <bek5056@gmail.com>
2024-01-26 08:57:36 +0500

    Modified

---
+++ git1.py
@@ -0,0 +1,11 @@
+'''lst = [12, 23, 34, 11, 111, 65, 34, 43]
+lst.sort()
+lst.reverse()
+print(lst)
+
+print(lst.index(12))'''
+
+lst = [10, 5, 8, 20, 3, 15]
+sorted_lst = sorted(lst, reverse=True)
+max3 = sorted_lst[:3]
+print(max3) 
