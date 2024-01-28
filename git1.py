2aa1d56ef1fc51931fdbe1bc6fa8f9b81e843206
ziko <157778657+realziko@users.noreply.github.com>
2024-01-29 01:10:29 +0500

    Update gitpy1.py

--- git1.py
+++ git1.py
@@ -1,5 +1,26 @@
-lst = [10, 5, 8, 20, 3, 15]
-sorted = sorted(lst, reverse=True)
-max3 = sorted[:3]
-print(sorted)
-print(max3) 
+def min_platforms(arr, dep):
+    n = len(arr)
+    arr.sort()
+    dep.sort()
+
+    platformn = 1
+    res = 1
+    i = 1
+    j = 0
+
+    while i < n and j < n:
+        if arr[i] <= dep[j]:
+            platformn += 1
+            i += 1
+            if platformn > res:
+                res = platformn
+        else:
+            platformn -= 1
+            j += 1
+
+    return res
+
+n = 6
+arr = [900, 940, 950, 1100, 1500, 1800]
+dep = [910, 1200, 1120, 1130, 1900, 2000]
+print(min_platforms(arr, dep))
